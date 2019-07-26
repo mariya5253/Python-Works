465884002f93f4723715be93094c0759671e35f7
mariya5253 <48204082+mariya5253@users.noreply.github.com>
2019-07-26 14:59:53 +0530

    Add files via upload

---
+++ GHCI AI-CODEATHON 2019/Codeathon/main1.py
@@ -0,0 +1,130 @@
+from flask import Flask, render_template
+import cv2
+from align_custom import AlignCustom
+from face_feature import FaceFeature
+from mtcnn_detect import MTCNNDetect
+from tf_graph import FaceRecGraph
+import argparse
+import sys
+import json
+import time
+import numpy as np
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+    
+@app.route('/input')
+def create_manual_data():
+    
+    FRGraph = FaceRecGraph();
+    MTCNNGraph = FaceRecGraph();
+    aligner = AlignCustom();
+    extract_feature = FaceFeature(FRGraph)
+    face_detect = MTCNNDetect(MTCNNGraph, scale_factor=2); #scale_factor, rescales image for faster detection
+    vs = cv2.VideoCapture(0); #get input from webcam
+#     print("Please input new user ID:")
+    new_name = request.form['input_name']; #ez python input()
+    f = open('./facerec_128D.txt','r');
+    data_set = json.loads(f.read());
+    person_imgs = {"Left" : [], "Right": [], "Center": []};
+    person_features = {"Left" : [], "Right": [], "Center": []};
+    print("Please start turning slowly. Press 'q' to save and add this new user to the dataset");
+    while True:
+        _, frame = vs.read();
+        rects, landmarks = face_detect.detect_face(frame, 80);  # min face size is set to 80x80
+        for (i, rect) in enumerate(rects):
+            aligned_frame, pos = aligner.align(160,frame,landmarks[:,i]);
+            if len(aligned_frame) == 160 and len(aligned_frame[0]) == 160:
+                person_imgs[pos].append(aligned_frame)
+                cv2.imshow("Captured face", aligned_frame)
+        key = cv2.waitKey(1) & 0xFF
+        if key == ord("q"):
+            break
+
+    for pos in person_imgs: #there r some exceptions here, but I'll just leave it as this to keep it simple
+        person_features[pos] = [np.mean(extract_feature.get_features(person_imgs[pos]),axis=0).tolist()]
+    data_set[new_name] = person_features;
+    f = open('./facerec_128D.txt', 'w');
+    f.write(json.dumps(data_set))
+
+    return render_template('home.html')
+    
+
+@app.route('/verify')
+def camera_recog():
+    FRGraph = FaceRecGraph();
+    MTCNNGraph = FaceRecGraph();
+    aligner = AlignCustom();
+    extract_feature = FaceFeature(FRGraph)
+    face_detect = MTCNNDetect(MTCNNGraph, scale_factor=2); #scale_factor, rescales image for faster detection
+    print("[INFO] camera sensor warming up...")
+    vs = cv2.VideoCapture(0); #get input from webcam
+    detect_time = time.time()
+    while True:
+        _,frame = vs.read();
+        #u can certainly add a roi here but for the sake of a demo i'll just leave it as simple as this
+        rects, landmarks = face_detect.detect_face(frame,80);#min face size is set to 80x80
+        aligns = []
+        positions = []
+
+        for (i, rect) in enumerate(rects):
+            aligned_face, face_pos = aligner.align(160,frame,landmarks[:,i])
+            if len(aligned_face) == 160 and len(aligned_face[0]) == 160:
+                aligns.append(aligned_face)
+                positions.append(face_pos)
+            else: 
+                print("Align face failed") #log        
+        if(len(aligns) > 0):
+            features_arr = extract_feature.get_features(aligns)
+            recog_data = findPeople(features_arr,positions)
+            for (i,rect) in enumerate(rects):
+                cv2.rectangle(frame,(rect[0],rect[1]),(rect[2],rect[3]),(255,0,0)) #draw bounding box for the face
+                cv2.putText(frame,recog_data[i][0]+" - "+str(recog_data[i][1])+"%",(rect[0],rect[1]),cv2.FONT_HERSHEY_SIMPLEX,1,(255,255,255),1,cv2.LINE_AA)
+
+
+        cv2.imshow("Frame",frame)
+        key = cv2.waitKey(1) & 0xFF
+        if key == ord("q"):
+            break
+            
+    return render_template('home.html')
+
+            
+            
+def findPeople(features_arr, positions, thres = 0.6, percent_thres = 70):
+    '''
+    :param features_arr: a list of 128d Features of all faces on screen
+    :param positions: a list of face position types of all faces on screen
+    :param thres: distance threshold
+    :return: person name and percentage
+    '''
+    TIMEOUT = 10 #10 seconds
+
+    f = open('./facerec_128D.txt','r')
+    data_set = json.loads(f.read());
+    returnRes = [];
+    for (i,features_128D) in enumerate(features_arr):
+        result = "Unknown";
+        smallest = sys.maxsize
+        for person in data_set.keys():
+            person_data = data_set[person][positions[i]];
+            for data in person_data:
+                distance = np.sqrt(np.sum(np.square(data-features_128D)))
+                if(distance < smallest):
+                    smallest = distance;
+                    result = person;
+        percentage =  min(100, 100 * thres / smallest)
+        if percentage <= percent_thres :
+            result = "Unknown"
+        returnRes.append((result,percentage))
+    return returnRes    
+
+
+
+
+if __name__ == '__main__':
+    app.run()
